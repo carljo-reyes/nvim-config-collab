5ce1022d7e20adb6f6c7d81b8ec61329e96bc659
Carljo Reyes <work.calreyes@gmail.com>
2024-02-02 22:13:32 +0800

    remap quit vim stuff

--- lua/cal/plugin/keymaps.lua
+++ lua/cal/plugin/keymaps.lua
@@ -1,11 +1,15 @@
 vim.keymap.set('n', '<M-e>', [[<cmd>E<cr>]])        -- explorer
 
+-----------------------------------------------------------
 -- tabs
+-----------------------------------------------------------
 vim.keymap.set('n', '<M-n>', [[<cmd>tabnew<cr>]])
 vim.keymap.set('n', '<M-,>', [[<cmd>tabprev<cr>]])
 vim.keymap.set('n', '<M-.>', [[<cmd>tabnext<cr>]])
 
+-----------------------------------------------------------
 -- panes
+-----------------------------------------------------------
 vim.keymap.set('', '<M-->', [[<cmd>new<cr>]])
 vim.keymap.set('', '<M-\\>', [[<cmd>vnew<cr>]])
 vim.keymap.set('', '<M-h>', [[<C-w>h]])
@@ -13,8 +17,13 @@ vim.keymap.set('', '<M-j>', [[<C-w>j]])
 vim.keymap.set('', '<M-k>', [[<C-w>k]])
 vim.keymap.set('', '<M-l>', [[<C-w>l]])
 
--- quit
+-----------------------------------------------------------
+-- close vim :)
+-----------------------------------------------------------
 vim.keymap.set('n', '<M-X>', [[<cmd>x<cr>]])
-vim.keymap.set('n', '<M-q>', [[<cmd>bd<cr>]])
-vim.keymap.set('n', '<M-Q>', [[<cmd>q!<cr>]])
-vim.keymap.set('n', '<M-W>', [[<cmd>qa!<cr>]])
+
+vim.keymap.set('n', '<M-w>', [[<cmd>bd<cr>]])
+vim.keymap.set('n', '<M-W>', [[<cmd>bd!<cr>]])
+
+vim.keymap.set('n', '<M-q>', [[<cmd>qa<cr>]])
+vim.keymap.set('n', '<M-Q>', [[<cmd>qa!<cr>]])
