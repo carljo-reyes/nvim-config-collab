aaa46b659f5bd5936ff1125e691f67e3f2c8d6f7
Carljo Reyes <work.calreyes@gmail.com>
2024-02-14 23:51:34 +0800

    add keymaps.lua

--- lua/cal/plugin/keymaps.lua
+++ lua/cal/plugin/keymaps.lua
@@ -1,29 +1,42 @@
-vim.keymap.set('n', '<M-e>', [[<cmd>E<cr>]])        -- explorer
+local cmd = function(command)
+    return '<cmd>' .. command .. '<cr>'
+end
 
------------------------------------------------------------
+vim.keymap.set('n', '<leader>l', cmd "Lazy")
+vim.keymap.set('n', '<leader>L', cmd "LspInfo")
+
+----------------------------------------------------------------
+-- editor
+----------------------------------------------------------------
+vim.keymap.set('v', '<cr>', [["+y]])
+vim.keymap.set('v', '<C-c>', [["+y]])
+vim.keymap.set('', '<M-e>', cmd "set nohls!")
+
+----------------------------------------------------------------
 -- tabs
------------------------------------------------------------
-vim.keymap.set('n', '<M-n>', [[<cmd>tabnew<cr>]])
-vim.keymap.set('n', '<M-,>', [[<cmd>tabprev<cr>]])
-vim.keymap.set('n', '<M-.>', [[<cmd>tabnext<cr>]])
+----------------------------------------------------------------
+vim.keymap.set('n', '<M-n>', cmd "tabnew")
+vim.keymap.set('n', '<M-,>', cmd "tabprev")
+vim.keymap.set('n', '<M-.>', cmd "tabnext")
 
------------------------------------------------------------
+----------------------------------------------------------------
 -- panes
------------------------------------------------------------
-vim.keymap.set('', '<M-->', [[<cmd>new<cr>]])
-vim.keymap.set('', '<M-\\>', [[<cmd>vnew<cr>]])
-vim.keymap.set('', '<M-h>', [[<C-w>h]])
-vim.keymap.set('', '<M-j>', [[<C-w>j]])
-vim.keymap.set('', '<M-k>', [[<C-w>k]])
-vim.keymap.set('', '<M-l>', [[<C-w>l]])
+----------------------------------------------------------------
+vim.keymap.set('', '<M-->', cmd "new")
+vim.keymap.set('', '<M-=>', cmd "vnew")
+-- disabled in favor of alexghergh/nvim-tmux-navigation plugin:
+-- vim.keymap.set('', '<M-h>', [[<C-w>h]])
+-- vim.keymap.set('', '<M-j>', [[<C-w>j]])
+-- vim.keymap.set('', '<M-k>', [[<C-w>k]])
+-- vim.keymap.set('', '<M-l>', [[<C-w>l]])
 
 -----------------------------------------------------------
 -- close vim :)
 -----------------------------------------------------------
-vim.keymap.set('n', '<M-X>', [[<cmd>x<cr>]])
+vim.keymap.set('n', '<M-X>', cmd "x")
 
-vim.keymap.set('n', '<M-w>', [[<cmd>bd<cr>]])
-vim.keymap.set('n', '<M-W>', [[<cmd>bd!<cr>]])
+vim.keymap.set('n', '<M-w>', cmd "bd")
+vim.keymap.set('n', '<M-W>', cmd "bd!")
 
-vim.keymap.set('n', '<M-q>', [[<cmd>qa<cr>]])
-vim.keymap.set('n', '<M-Q>', [[<cmd>qa!<cr>]])
+vim.keymap.set('n', '<M-q>', cmd "q")
+vim.keymap.set('n', '<M-Q>', cmd "qa!")
