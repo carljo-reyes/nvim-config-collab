8f16e1463042ddb91250c041ba2725cd51fe0d17
Carljo Rommel Reyes <carljo.reyes@collaberadigital.com>
2025-02-27 19:34:47 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,22 +2,29 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 vim.g.maplocalleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {
+require("lazy").setup({
+    spec = {
+        import = "lazy-plugins"
+    },
     change_detection = {
-        enabled = true, notify = false
+        enabled = true,
+        notify = false
     },
     git = {
         timeout = 300
@@ -33,8 +40,4 @@ local opts = {
             }
         }
     },
-}
-
-require("lazy").setup({
-    { import = "lazy-plugins" },
-}, opts)
+});
