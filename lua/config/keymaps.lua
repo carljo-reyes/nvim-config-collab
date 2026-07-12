523f250e0a4890f5561637b816cafebc20b45978
Carljo Rommel Reyes <work.calreyes@gmail.com>
2026-07-12 15:53:08 +0800

    add keymaps for tabs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,8 @@ vim.keymap.set("t", "<ESC>", [[<C-\><C-N>]])
 vim.keymap.set("n", "<M-i>", cmd("bprevious"))
 vim.keymap.set("n", "<M-o>", cmd("bnext"))
 vim.keymap.set("v", "<Enter>", '"+y')
+
+vim.keymap.set("n", "<Tab>[", cmd("tabprevious"))
+vim.keymap.set("n", "<Tab>]", cmd("tabnext"))
+vim.keymap.set("n", "<Tab>n", cmd("tab split"))
+vim.keymap.set("n", "<Tab>q", cmd("tabclose"))
