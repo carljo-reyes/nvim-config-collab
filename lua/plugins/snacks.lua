f6b137349c8531e0d6c1d164497b1ddee6b8ee4f
Carljo Rommel Reyes <zcarljorommel.reyes@gcash.com>
2026-07-15 00:50:24 +0800

    snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,20 +2,29 @@ return {
   "folke/snacks.nvim",
   opts = function(_, opts)
     local keys = opts.dashboard.preset.keys
-    table.insert(keys, {
-      {
-        hidden = true,
-        icon = " ",
-        key = "i",
-        desc = "New Scratch",
-        action = ":ene | set buftype=nofile | startinsert",
-      },
-    })
+    -- commented for reference
+    -- table.insert(keys, {
+    --   {
+    --     hidden = true,
+    --     icon = " ",
+    --     key = "i",
+    --     desc = "New Scratch",
+    --     action = ":ene | set buftype=nofile | startinsert",
+    --   },
+    -- })
     for i, item in ipairs(keys) do
+      -- change default "f" -> "o"
+      if item.key == "f" then
+        item.key = "o"
+      end
+
+      -- change default "n" -> "i"
       if item.key == "n" then
+        item.key = "i"
         opts.dashboard.preset.keys[i] = vim.tbl_extend("force", item, {
-          desc = "New Scratch",
+          -- open scratch buffer instead
           action = ":ene | set buftype=nofile | startinsert",
+          desc = "New Scratch",
         })
         break
       end
