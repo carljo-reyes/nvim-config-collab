31bc3243ed07f3b27e136fd2f6b346b6d0962e2d
Carljo Rommel Reyes <zcarljorommel.reyes@gcash.com>
2026-07-16 17:34:21 +0800

    remove vim.g.dbs, and set relativenumber to false

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,11 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.dbs = {
-  localhost = "mysql://127.0.0.1:3306/payparkph",
-}
-
 vim.opt.clipboard = ""
 vim.opt.wrap = true
+vim.opt.relativenumber = false
 
 vim.g.autoformat = false
