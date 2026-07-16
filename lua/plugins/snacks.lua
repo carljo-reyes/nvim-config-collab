20e1e13378192d1da78e6199e0bc521d135ab8e2
Carljo Rommel Reyes <work.calreyes@gmail.com>
2026-07-17 01:48:25 +0800

    update snacks keys

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,33 +1,29 @@
+local replace = function(keys, i, prev_key, new_key, opts)
+  local item = keys[i]
+  if item.key == prev_key then
+    item.key = new_key
+    keys[i] = vim.tbl_extend("force", item, opts or {})
+  end
+end
+
 return {
   "folke/snacks.nvim",
   opts = function(_, opts)
     local keys = opts.dashboard.preset.keys
-    -- commented for reference
-    -- table.insert(keys, {
-    --   {
-    --     hidden = true,
-    --     icon = " ",
-    --     key = "i",
-    --     desc = "New Scratch",
-    --     action = ":ene | set buftype=nofile | startinsert",
-    --   },
-    -- })
-    for i, item in ipairs(keys) do
-      -- change default "f" -> "o"
-      if item.key == "f" then
-        item.key = "o"
-      end
-
-      -- change default "n" -> "i"
-      if item.key == "n" then
-        item.key = "i"
-        opts.dashboard.preset.keys[i] = vim.tbl_extend("force", item, {
-          -- open scratch buffer instead
-          action = ":ene | set buftype=nofile | startinsert",
-          desc = "New Scratch",
-        })
-        break
-      end
+    for i, _ in ipairs(keys) do
+      replace(keys, i, "g", "/")
+      replace(keys, i, "n", "i", {
+        action = ":ene | set buftype=nofile | startinsert",
+        desc = "New Scratch",
+      })
     end
+
+    table.insert(keys, {
+      {
+        hidden = true,
+        key = "o",
+        action = ":lua Snacks.dashboard.pick('files')",
+      },
+    })
   end,
 }
