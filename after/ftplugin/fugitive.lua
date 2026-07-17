03779922a2ffaa3addb928d54ee667ef07935e6d
Carljo Rommel Reyes <work.calreyes@gmail.com>
2026-07-17 23:21:56 +0800

    fix fugitive cc

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -2,7 +2,8 @@ local cmd = require("cal.mapping").cmd
 local map = require("cal.mapping").map
 
 map("n", "q", cmd("bd"))
-map("n", "cc", "<cmd>Git commit -v<CR>", {
+map("n", "cc", cmd("Git commit -v"), {
+  buffer = 0,
   silent = true,
   desc = "commit",
 })
